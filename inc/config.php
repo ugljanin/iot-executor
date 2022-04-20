9ec38f81ffa00fbbefda656a4c6256ee0c0edd52
Emir Ugljanin <emirugljanin@gmail.com>
2022-04-20 12:42:39 +0200

    Added config file

---
+++ inc/config.php
@@ -0,0 +1,21 @@
+<?php
+if(!defined('Functions')){die('You are not authorised to access this');}
+/**
+ * Configuration file for MQTT server and MYSQL server.
+ *
+ * @package B2S4B
+ */
+
+// These variables define the connection information for your MySQL database.
+$db_user = '';
+$db_pass = '';
+$db_host = 'localhost';
+$db_name = '';
+
+// These variables define the connection information for your MQTT server.
+$mqtt_host   = '';
+$mqtt_user   = '';
+$mqtt_pass   = '';
+$mqtt_port   = 1883;
+$mqtt_client = 'b2s4b';
+
